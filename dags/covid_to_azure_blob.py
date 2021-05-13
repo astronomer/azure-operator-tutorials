2085f93a79a6545f9f22222d6398af9febbeec08
Josh Fell <josh.d.fell@astronomer.io>
2021-05-13 15:27:29 -0400

    Removing example-dag and personal email addresses.

--- dags/covid_to_azure_blob.py
+++ dags/covid_to_azure_blob.py
@@ -11,56 +11,58 @@ import requests
 
 def upload_to_azure_blob(endpoint, date):
     # Instanstiate
-    azurehook = WasbHook(wasb_conn_id='azure_blob')
+    azurehook = WasbHook(wasb_conn_id="azure_blob")
 
     # Make request to get Covid data from API
-    url = 'https://covidtracking.com/api/v1/states/'
-    filename = '{0}/{1}.csv'.format(endpoint, date)
-    res = requests.get(url+filename)
+    url = "https://covidtracking.com/api/v1/states/"
+    filename = "{0}/{1}.csv".format(endpoint, date)
+    res = requests.get(url + filename)
 
     # Take string, upload to S3 using predefined method
-    azurehook.load_string(string_data=res.text, container_name='covid-data', blob_name=filename)
+    azurehook.load_string(
+        string_data=res.text, container_name="covid-data", blob_name=filename
+    )
 
 
 # Default settings applied to all tasks
 default_args = {
-    'owner': 'airflow',
-    'depends_on_past': False,
-    'email_on_failure': False,
-    'email_on_retry': False,
-    'retries': 1,
-    'retry_delay': timedelta(minutes=1)
+    "owner": "airflow",
+    "depends_on_past": False,
+    "email_on_failure": False,
+    "email_on_retry": False,
+    "retries": 1,
+    "retry_delay": timedelta(minutes=1),
 }
 
-endpoints = ['ca', 'co', 'ny', 'pa', 'wa']
-date = '{{ ds_nodash }}'
-email_to = ["kenten@astronomer.io"]
+endpoints = ["ca", "co", "ny", "pa", "wa"]
+date = "{{ ds_nodash }}"
+email_to = ["example@example.com"]
 
 
-with DAG('covid_data_to_azure_blob',
-         start_date=datetime(2020, 12, 1),
-         max_active_runs=1,
-         schedule_interval='@daily',
-         default_args=default_args,
-         catchup=False  # enable if you don't want historical dag runs to run
-         ) as dag:
+with DAG(
+    "covid_data_to_azure_blob",
+    start_date=datetime(2020, 12, 1),
+    max_active_runs=1,
+    schedule_interval="@daily",
+    default_args=default_args,
+    catchup=False,  # enable if you don't want historical dag runs to run
+) as dag:
 
-
-    t0 = DummyOperator(task_id='start')
+    t0 = DummyOperator(task_id="start")
 
     send_email = EmailOperator(
-        task_id='send_email',
+        task_id="send_email",
         to=email_to,
-        subject='Covid to Azure Blob DAG',
-        html_content='<p>The Covid to Azure Blob DAG completed successfully. Files can now be found in Azure blob storage. <p>'
+        subject="Covid to Azure Blob DAG",
+        html_content="<p>The Covid to Azure Blob DAG completed successfully. Files can now be found in Azure blob storage. <p>",
     )
-    
-    with TaskGroup('covid_task_group') as covid_group:
+
+    with TaskGroup("covid_task_group") as covid_group:
         for endpoint in endpoints:
             generate_files = PythonOperator(
-                task_id='generate_file_{0}'.format(endpoint),
+                task_id="generate_file_{0}".format(endpoint),
                 python_callable=upload_to_azure_blob,
-                op_kwargs={'endpoint': endpoint, 'date': date}
+                op_kwargs={"endpoint": endpoint, "date": date},
             )
-        
+
     t0 >> covid_group >> send_email
