79bc0e8b62eea924ff9bcee9e795f6c51085810e
Josh Fell <josh.d.fell@astronomer.io>
2021-08-26 13:56:53 -0400

    Updating to use TaskFlow API

--- dags/azure-data-factory-dag.py
+++ dags/azure-data-factory-dag.py
@@ -1,6 +1,6 @@
 from airflow import DAG
+from airflow.decorators import task
 from datetime import datetime, timedelta
-from airflow.operators.python_operator import PythonOperator
 from airflow.providers.microsoft.azure.hooks.azure_data_factory import AzureDataFactoryHook
 
 azure_data_factory_conn = 'azure_data_factory_conn'
@@ -8,10 +8,9 @@ azure_data_factory_conn = 'azure_data_factory_conn'
 #Get yesterday's date, in the correct format
 yesterday_date = '{{ yesterday_ds_nodash }}'
 
-
-def run_adf_pipeline(pipeline_name, date):
-    '''Runs an Azure Data Factory pipeline using the AzureDataFactoryHook and passes in a date parameter
-    '''
+@task
+def run_adf_pipeline(pipeline_name: str, date: str) -> None:
+    '''Runs an Azure Data Factory pipeline using the AzureDataFactoryHook and passes in a date parameter'''
 
     #Create a dictionary with date parameter
     params = {}
@@ -35,8 +34,4 @@ with DAG(
     catchup=False
 ) as dag:
 
-         opr_run_pipeline = PythonOperator(
-            task_id='run_pipeline',
-            python_callable=run_adf_pipeline,
-            op_kwargs={'pipeline_name': 'pipeline1', 'date': yesterday_date}
-         )
+    run_adf_pipeline(pipeline_name="pipeline1", date=yesterday_date)
--- dags/covid_to_azure_blob.py
+++ dags/covid_to_azure_blob.py
@@ -1,27 +1,12 @@
 from airflow import DAG
+from airflow.decorators import task
 from airflow.operators.dummy_operator import DummyOperator
 from airflow.operators.email_operator import EmailOperator
-from airflow.operators.python_operator import PythonOperator
 from airflow.providers.microsoft.azure.hooks.wasb import WasbHook
 from airflow.utils.task_group import TaskGroup
 from datetime import datetime, timedelta
 import os
 import requests
-
-
-def upload_to_azure_blob(endpoint, date):
-    # Instanstiate
-    azurehook = WasbHook(wasb_conn_id="azure_blob")
-
-    # Make request to get Covid data from API
-    url = "https://covidtracking.com/api/v1/states/"
-    filename = "{0}/{1}.csv".format(endpoint, date)
-    res = requests.get(url + filename)
-
-    # Take string, upload to S3 using predefined method
-    azurehook.load_string(
-        string_data=res.text, container_name="covid-data", blob_name=filename
-    )
 
 
 endpoints = ["ca", "co", "ny", "pa", "wa"]
@@ -35,10 +20,10 @@ with DAG(
     max_active_runs=1,
     schedule_interval="@daily",
     default_args={
-        'email_on_failure': False,
-        'email_on_retry': False,
-        'retries': 1,
-        'retry_delay': timedelta(minutes=1)
+        "email_on_failure": False,
+        "email_on_retry": False,
+        "retries": 1,
+        "retry_delay": timedelta(minutes=1),
     },
     catchup=False,  # enable if you don't want historical dag runs to run
 ) as dag:
@@ -54,10 +39,20 @@ with DAG(
 
     with TaskGroup("covid_task_group") as covid_group:
         for endpoint in endpoints:
-            generate_files = PythonOperator(
-                task_id="generate_file_{0}".format(endpoint),
-                python_callable=upload_to_azure_blob,
-                op_kwargs={"endpoint": endpoint, "date": date},
-            )
+
+            @task(task_id="generate_file_{0}".format(endpoint))
+            def upload_to_azure_blob(endpoint: str, date: str, **kwargs) -> None:
+                # Instanstiate
+                azurehook = WasbHook(wasb_conn_id="azure_blob")
+
+                # Make request to get Covid data from API
+                url = "https://covidtracking.com/api/v1/states/"
+                filename = "{0}/{1}.csv".format(endpoint, date)
+                res = requests.get(url + filename)
+
+                # Take string, upload to S3 using predefined method
+                azurehook.load_string(string_data=res.text, container_name="covid-data", blob_name=filename)
+
+            upload_to_azure_blob(endpoint=endpoint, date=date)
 
     t0 >> covid_group >> send_email
